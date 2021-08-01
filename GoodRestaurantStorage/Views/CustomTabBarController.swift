8f7742655a0d6e421201b759f38b71e79d4bd7aa
dwkang <ehddn3821@naver.com>
2021-08-01 23:20:59 +0900

    Feat: Login session check on AddPostViewController

--- GoodRestaurantStorage/Views/CustomTabBarController.swift
+++ GoodRestaurantStorage/Views/CustomTabBarController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CustomTabBarController: UITabBarController {
+class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
     
     var mainTabBar: MainViewController = {
         let vc = MainViewController()
@@ -46,6 +46,7 @@ class CustomTabBarController: UITabBarController {
         
         setupTabBarShadow(tabBar)
         tabBar.tintColor = .black
+        self.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -53,6 +54,24 @@ class CustomTabBarController: UITabBarController {
         
         navigationController?.setNavigationBarHidden(true, animated: animated)
         viewControllers = [mainTabBar, addPostTabBar, profileTabBar]
-        selectedIndex = 1
+        selectedIndex = 2
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        // 맛집 추가 탭은 로그인 세션 체크
+        if viewController == addPostTabBar {
+            if !UserDefaults.standard.bool(forKey: "user") {
+                let alert = UIAlertController(title: "로그인이 필요합니다.", message: "", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+                    self.selectedIndex = 2
+                }
+                alert.addAction(okAction)
+                present(alert, animated: true, completion: nil)
+                
+                return false
+            }
+        }
+        return true
     }
 }
